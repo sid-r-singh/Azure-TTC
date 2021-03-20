9142a1a9ec0bd863cedeaa4942dfc1fb5d0e5c3d
sid-r-singh <ofw3210@gmail.com>
2021-03-20 19:33:52 +0530

    Integrated firebase

--- GH-FB-HTTP-trigger/__init__.py
+++ GH-FB-HTTP-trigger/__init__.py
@@ -1,5 +1,10 @@
 import logging
 import json
+import pyrebase
+import requests
+import pytz
+import os
+from datetime import datetime
 import azure.functions as func
 
 
@@ -10,7 +15,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     body = req.get_json()
     print(body)
     print(type(body))
-    sim_type=body['sim']
+    sim_type=body['sim_name']
     if sim_type=="MC":
         print("Detected MC sim")
         if type(body['trials']=='int' and type(body['user_name'])=='string'):
@@ -19,6 +24,40 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             
             #Upon success, add IST time & send to FB
             
+            #FB setup
+            config = {
+            "apiKey": os.environ["fb_apiKey"],
+            "authDomain": os.environ["fb_authDomain"],
+            "databaseURL": os.environ["fb_databaseURL"],
+            "storageBucket": os.environ["fb_storageBucket"]
+            }
+            firebase = pyrebase.initialize_app(config)
+            auth = firebase.auth()
+
+            # Log the user in
+            user = auth.sign_in_with_email_and_password('abc@xyz.com', 'abc@xyz.com')
+            url = 'https://mt-iot-brn.firebaseio.com/tg-gh-fb-logs.json?auth='+ user['idToken']
+            headers = {
+            'Content-Type': 'application/json'
+            }
+            
+            #Add time info to payload
+            time_zone = pytz.timezone('Asia/Kolkata')
+            current_ts = datetime.now(time_zone) 
+            time_string = current_ts.strftime('%d-%m-%Y %H:%M:%S')
+            body['IST_time'] = time_string
+            
+            #Send data to FB
+            data = body
+            response = requests.request(
+            'POST',
+            url,
+            json=data,
+            )
+
+            #Print server response
+            print(response)
+            print(response.json())
             
         else:
             print("All is not good")
