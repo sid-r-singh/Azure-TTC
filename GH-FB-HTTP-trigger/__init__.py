059af70fa2fb3c48216b322634c0d1ff9d3fedf9
Sidharth R <ofw3210@gmail.com>
2021-09-26 23:10:57 +0530

    Update __init__.py

--- GH-FB-HTTP-trigger/__init__.py
+++ GH-FB-HTTP-trigger/__init__.py
@@ -51,43 +51,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             if status==204:
                 print('Success')
             else:
-                print('Faileeeed')
+                print('Failed')
 
-        #Upon success, add IST time & send to FB
-            
-            #FB setup
-            # config = {
-            # "apiKey": os.environ["fb_apiKey"],
-            # "authDomain": os.environ["fb_authDomain"],
-            # "databaseURL": os.environ["fb_databaseURL"],
-            # "storageBucket": os.environ["fb_storageBucket"]
-            # }
-            # firebase = pyrebase.initialize_app(config)
-            # auth = firebase.auth()
-
-            # Log the user in
-            # fb_user = os.environ["fb_user_email"]
-            # fb_pswd = os.environ["fb_pswd"]
-            # user = auth.sign_in_with_email_and_password(fb_user, fb_pswd)
-            # url = 'https://mt-iot-brn.firebaseio.com/tg-gh-fb-logs.json?auth='+ user['idToken']
-            # headers = {
-            # 'Content-Type': 'application/json'
-            # }
-            
             #Add time info to payload
             time_zone = pytz.timezone('Asia/Kolkata')
             current_ts = datetime.now(time_zone) 
             time_string = current_ts.strftime('%d-%m-%Y %H:%M:%S')
             body['IST_time'] = time_string
-
-            
-            #Send data to FB
-            # data = body
-            # response = requests.request(
-            # 'POST',
-            # url,
-            # json=data,
-            # )
 
             #Send data to TG
             url = 'https://api.telegram.org/bot'+str(os.environ['tg_bot_token'])+'/sendMessage'
